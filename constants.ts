b48bb63293b7cacc4fc5c72a3fc014e0bb90e1f5
Amogh Masna <amoghmasna@gmail.com>
2026-08-13 18:50:24 +0530

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const committees=['DISEC','UNHRC','International Press','Lok Sabha','FIFA','UNSC'] as const;
+export type Committee=typeof committees[number];
+export const committeeInfo:Record<Committee,string>={DISEC:'Disarmament and International Security',UNHRC:'United Nations Human Rights Council','International Press':'International Press Corps','Lok Sabha':'The House of the People',FIFA:'F�d�ration Internationale de Football Association',UNSC:'United Nations Security Council'};
+export const departments=['Delegate Affairs','Logistics','Media & Design','Marketing & Outreach','Technical Team'];
